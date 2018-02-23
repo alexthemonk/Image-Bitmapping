dccd5ad5e94725d7294cacd21a7ac56efc5de367
alexthemonk <30848036+alexthemonk@users.noreply.github.com>
2018-02-23 15:15:32 -0500

    read from command line argument

--- main.cpp
+++ main.cpp
@@ -8,7 +8,13 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char * argv[]) {
+    
+    if(argc != 2){
+        cout << "Error: number of input argument must be 1" << endl;
+        exit(1);
+    }
+    
     int choice = 0;
     int width, height;
 
@@ -18,19 +24,19 @@ int main() {
     cin >> height;
 
     Image pic(width, height);
-    pic.readRawRGBFile("/home/lius214/Lab1/home800_286.raw");
+    pic.readRawRGBFile(string(argv[1]));
     //pic.printArray();
     cout << "Choose an option" << endl << "1 for popularity, 2 for uniform partitioning" << endl;
     cin >> choice;
     cout << endl;
     if(choice == 1){
         cout << "producing image using Popularity Algorithm: outputPopularity.bmp" << endl;
-        pic.setFilePath("/home/lius214/Lab1/home800_286Popularity.bmp");
+        pic.setFilePath(string(argv[1]) + "Popularity.bmp");
         pic.popularity();
     }
     else{
         cout << "producing image using Uniform Partitioning Algorithm: outputUniform.bmp" << endl;
-        pic.setFilePath("/home/lius214/Lab1/home800_286Uniform.bmp");
+        pic.setFilePath(string(argv[1]) + "Uniform.bmp");
         pic.uniformPartitioning();
     }
     pic.createIndexedFile(choice);
